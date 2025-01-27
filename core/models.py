18a68885bf7007946611ee3464e9318d49d818aa
Elvin <elvinhaxverdiyev777@gamil.com>
2025-01-27 12:59:18 +0400

    models Configuration

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Home(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='profile_pics/')  # Resimlerin yükleneceği klasör
+    image = models.ImageField(upload_to='profile_pics/')
     rol1 = models.CharField(max_length=20, null=True, blank=True)
     rol2 = models.CharField(max_length=20, null=True, blank=True)
     rol3 = models.CharField(max_length=20, null=True, blank=True)
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Home, About
 
 def home(request):
-    home_items = Home.objects.all()  # Home verilerini alıyoruz
-    about_item = About.objects.first()  # About modelinin ilk kaydını alıyoruz
+    home_items = Home.objects.all() 
+    about_item = About.objects.first() 
 
     return render(request, 'home.html', {'home_items': home_items, 'about_item': about_item})
